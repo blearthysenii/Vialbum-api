435e3eb9c85816011045ddd9cd978cf0057d0696
blehtt <bleart.hyseni@umib.net>
2026-09-01 13:33:57 +0200

    Register places routes

--- app/api/router.py
+++ app/api/router.py
@@ -4,6 +4,7 @@ from app.api.routes.auth import router as auth_router
 from app.api.routes.journeys import router as journeys_router
 from app.api.routes.map import router as map_router
 from app.api.routes.memories import router as memories_router
+from app.api.routes.places import router as places_router
 from app.api.routes.system import router as system_router
 
 api_router = APIRouter()
@@ -12,3 +13,4 @@ api_router.include_router(auth_router)
 api_router.include_router(journeys_router)
 api_router.include_router(map_router)
 api_router.include_router(memories_router)
+api_router.include_router(places_router)
